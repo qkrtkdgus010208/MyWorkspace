48f75922fbc4bc900f1fc9ca3758b343f97acbe4
qkrtkdgus010208 <psh010208@naver.com>
2025-04-06 23:22:48 +0900

    패키지 이름 수정
    패키지의 이름을 ch06_project_01로 수정

---
+++ ch06_project_01/src/main/java/ch06_project_01/ems/member/service/StudentRegisterService.java
@@ -0,0 +1,27 @@
+package ch06_project_01.ems.member.service;
+
+import ch06_project_01.ems.member.Student;
+import ch06_project_01.ems.member.dao.StudentDao;
+
+public class StudentRegisterService { // 학생 정보를 데이터베이스(지금은 HashMap)에 저장하는 기능
+	
+	private StudentDao studentDao;
+	
+	public StudentRegisterService(StudentDao studentDao) {
+		this.studentDao = studentDao;
+	}
+	
+	public void register(Student student) {
+		if (verify(student.getsNum())) {
+			studentDao.insert(student);
+		} else {
+			System.out.println("The student has already registered.");
+		}
+	}
+	
+	public boolean verify(String sNum) {
+		Student student = studentDao.select
+				(sNum);
+		return student == null ? true : false;
+	}
+}
---
+++ ch06_project_01/src/main/java/ch06_project_01/ems/member/service/StudentSelectService.java
@@ -0,0 +1,32 @@
+package ch06_project_01.ems.member.service;
+
+import ch06_project_01.ems.member.Student;
+import ch06_project_01.ems.member.dao.StudentDao;
+
+public class StudentSelectService {
+	
+	/* 학생 한 명의 정보를 얻는 기능
+	– 다른 Service 클래스와 마찬가지로 생성자에서 전달받은 DAO 객체를
+	studentDao 필드에 저장 */
+	
+	private StudentDao studentDao;
+	
+	public StudentSelectService(StudentDao studentDao) {
+		this.studentDao = studentDao;
+	}
+	
+	public Student select(String sNum) {
+		if (verify(sNum)) {
+			return studentDao.select(sNum);
+		} else {
+			System.out.println("Student information is not available.");
+		}
+		return null;
+	}
+	
+	public boolean verify(String sNum) {
+		Student student = studentDao.select
+				(sNum);
+		return student != null ? true : false;
+	}
+}
