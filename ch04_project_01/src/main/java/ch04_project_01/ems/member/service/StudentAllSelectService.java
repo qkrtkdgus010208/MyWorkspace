71f69d26b92f757c6abc0ecd6e8fbc349d49d03d
qkrtkdgus010208 <psh010208@naver.com>
2025-04-21 08:19:49 +0900

    3주차 2차시

---
+++ ch04_project_01/src/main/java/ch04_project_01/ems/member/service/StudentAllSelectService.java
@@ -0,0 +1,19 @@
+package ch04_project_01.ems.member.service;
+
+import java.util.Map;
+
+import ch04_project_01.ems.member.Student;
+import ch04_project_01.ems.member.dao.StudentDao;
+
+public class StudentAllSelectService {
+	
+	private StudentDao studentDao;
+	
+	public StudentAllSelectService(StudentDao studentDao) {
+		this.studentDao = studentDao;
+	}
+	
+	public Map<String, Student> allSelect() {
+		return studentDao.getStudentDB();
+	}
+}
